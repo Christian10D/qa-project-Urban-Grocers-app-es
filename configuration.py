d190d227c968f0ba97875d8dea4a46e962541e9d
Christian Palafox <59578726+Christian10D@users.noreply.github.com>
2025-01-03 12:58:51 -0600

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
+URL_SERVICE = https://cnt-9bad90ea-68fb-4769-996c-e69926e7351c.containerhub.tripleten-services.com
 CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
 KITS_PATH = # Almacena la ruta para crear un kit en esta variable
