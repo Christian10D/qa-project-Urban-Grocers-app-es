df4dd84c9842ea5379a72fac3cbdd6141684d98b
Christian Palafox <59578726+Christian10D@users.noreply.github.com>
2025-01-15 17:54:23 -0600

    configuration.py

--- configuration.py
+++ configuration.py
@@ -1,3 +1,6 @@
-URL_SERVICE = https://cnt-9bad90ea-68fb-4769-996c-e69926e7351c.containerhub.tripleten-services.com
-CREATE_USER_PATH = "/api/v1/users"
-KITS_PATH = "/api/v1/kits"
+URL_SERVICE = "https://cnt-dcd9181d-4218-4153-9fc6-449bb77160b8.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
